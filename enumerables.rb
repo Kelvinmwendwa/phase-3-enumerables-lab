dc69b4e49c1b811dfcbeb2f1df3980cc630fd78d
Kelvin <kmwendwa476@gmail.com>
2022-08-18 18:57:15 +0300

    commit tree 474ad82ec93e6fac4644e1c40f4ead3367f16b04

--- enumerables.rb
+++ enumerables.rb
@@ -13,13 +13,21 @@ end
 # with the names of each spicy food
 def get_names(spicy_foods)
   # your code here
+  spicy_foods.map do |food|
+    food[:name]
+  end
 end
+
 
 # given an array of spicy foods, **return an array of hashes** 
 # where the heat level of the food is greater than 5
 def spiciest_foods(spicy_foods)
   # your code here
+  spicy_foods.filter do |food|
+    food[:heat_level] > 5
+  end
 end
+
 
 # given an array of spicy foods, **output to the terminal**
 # each spicy food in the following format: 
@@ -28,7 +36,12 @@ end
 # "hello" * 3 == "hellohellohello"
 def print_spicy_foods(spicy_foods)
   # your code here
+  spicy_foods.each do |food|
+    spice_emoji = '🌶' * food[:heat_level]
+    puts "#{food[:name]} (#{food[:cuisine]}) | heat_level: #{spice_emoji}"
+  end
 end
+puts print_spicy_foods(spicy_foods)
 
 # given an array of spicy foods and a string representing a cuisine, **return a single hash**  
 # for the spicy food whose cuisine matches the cuisine being passed to the method
